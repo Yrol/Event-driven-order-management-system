d4929efd27b189dcb7b6d2f63c7065b95c07dd9d
Yrol <yrollgayanth@yahoo.com>
2023-09-26 22:42:43 +1000

    Configuring RabbitMQ

---
+++ order-service/src/main/java/blog/yrol/config/RabbiMQConfig.java
@@ -0,0 +1,28 @@
+package blog.yrol.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbiMQConfig {
+
+    /**
+     * Setting up / enabling JSON in RabbitTemplate to be compatible with JSON
+     * **/
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connection) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connection);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
